66ab58b9d16c0a9dd88bf5320d732a858a27ebee
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-03-24 16:54:44 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using IPLFranchise2021.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace IPLFranchise2021
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
