a40a4387663cf6f14971aa64e22f96cc6251ec08
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-03-24 21:58:21 +0530

    home view has been added

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Prism.Mvvm;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace IPLFranchise2021.ViewModels
 {
-    public class ShellViewModel :BindableBase
+    class HomeViewModel
     {
-
     }
 }
